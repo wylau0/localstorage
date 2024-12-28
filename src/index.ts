0f64ff04805bb60bc9e0eaa63eecff7218475fe8
WY Lau <git@wylau.com>
2024-12-28 19:32:44 +0000

    Added count & getAll(all)

--- src/index.ts
+++ src/index.ts
@@ -8,15 +8,42 @@
 'use strict';
 
 const set = <T = unknown>(key: string, value: T) : void => {
-  localStorage.setItem(key, JSON.stringify(value));
+  if (typeof value !== 'string') {
+    localStorage.setItem(key, JSON.stringify(value));
+  } else {
+    localStorage.setItem(key, value);
+  }
 };
+
+const count = () : number => {
+  return localStorage.length;
+}
 
 const get = (key: string) => {
   const value = localStorage.getItem(key);
 
   if (value === null) return null;
-  return JSON.parse(value);
+  try {
+    const val = JSON.parse(value);
+    return val;
+  } catch (e) {
+    return value;
+  }
 };
+
+const getAll = () => {
+    const items = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (key !== null) {
+        const value = get(key);
+        items.push({ key, value });
+      }
+    }
+    return items;
+}
+
+const all = getAll;
 
 const remove = (key: string) : void => {
   localStorage.removeItem(key);
@@ -28,7 +55,10 @@ const clear = () : void => {
 
 export default {
   set,
+  count,
   get,
+  getAll,
+  all,
   remove,
   clear
 };
